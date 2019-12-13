e0c2780b2b4ffbaa10b3af20152ba3c39731f14c
ChanHo Park <pch8286@unist.ac.kr>
2019-12-13 16:27:53 +0900

    Add status_minifi
    It run a command {MINIFI_BIN}/minifi.sh status

---
+++ scripts/status_minifi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+HOME="/home/ubuntu"
+MINIFI_DIR="$HOME/minifi"
+MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
+MINIFI_CONF="$MINIFI_HOME/conf"
+MINIFI_BIN="$MINIFI_HOME/bin"
+
+if [ -d "$MINIFI_DIR" ]; then
+	$MINIFI_BIN/minifi.sh status
+else
+	echo "$0: $MINIFI_DIR is missing."
+fi
