840d2729d9281d3b9c6ffd26ca48282b8dcbcae4
ChanHo Park <pch8286@unist.ac.kr>
2019-12-13 13:25:40 +0900

    Change permission and dependent dir
    ubuntu to root
    dir to ubuntu HOME

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,12 +14,11 @@ echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> $HOME/.bashrc;
 
 #check minifi dir and extract from minifi.tar.gz
 if [ ! -d "$HOME/minifi" ]; then
-        tar -xvf $HOME/minifi.tar.gz
+        cat $HOME/minifi.tar.gz.* | tar -zxvf -
+        mv minifi $HOME/minifi
 fi
 
 $MINIFI_DIR/minifi-toolkit/minifi-toolkit-0.5.0/bin/config.sh transform $HOME/mini-distr.xml $HOME/config.yml
 mv $HOME/config.yml $MINIFI_CONF
-
-chmod +x $HOME/scripts/*.sh
 
 . $HOME/.bashrc
