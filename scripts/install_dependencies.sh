c90c74ce92e1ee043931ac73e6aafd4a228a1532
pch8286 <pch8286@gmail.com>
2019-12-13 12:02:50 +0900

    Fix dir poistion
    C:/Users/pch82 dir is applied

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,16 +10,16 @@ MINIFI_BIN="$MINIFI_HOME/bin"
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 
-echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> ~/.bashrc;
+echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> $HOME/.bashrc;
 
 #check minifi dir and extract from minifi.tar.gz
 if [ ! -d "$HOME/minifi" ]; then
-        tar -xvf minifi.tar.gz
+        tar -xvf $HOME/minifi.tar.gz
 fi
 
-$MINIFI_DIR/minifi-toolkit/minifi-toolkit-0.5.0/bin/config.sh transform $HOME/mini-distr.xml config.yml
+$MINIFI_DIR/minifi-toolkit/minifi-toolkit-0.5.0/bin/config.sh transform $HOME/mini-distr.xml $HOME/config.yml
 mv $HOME/config.yml $MINIFI_CONF
 
 chmod +x $HOME/scripts/*.sh
 
-. ~/.bashrc
+. $HOME/.bashrc
