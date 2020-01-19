803337c9176148a81054f382f454d39b215193b0
ChanHo Park <pch8286@unist.ac.kr>
2020-01-19 16:26:43 +0900

    Add scalable minifi

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,17 +8,16 @@ MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
 
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
-
-echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> $HOME/.bashrc;
+sudo apt-get install openjdk-8-jdk
+echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+source /etc/environment
 
 #check minifi dir and extract from minifi.tar.gz
-if [ ! -d "$HOME/minifi" ]; then
+if [ ! -d "$HOME/jarvis-minifi-scale-aws" ]; then
         cat $HOME/minifi.tar.gz.* | tar -zxvf -
-        mv minifi $HOME/minifi
+        mv $HOME/jarvis-minifi-scale-aws/minifi $HOME/minifi
+        sudo rm -r $HOME/jarvis-minifi-scale-aws
 fi
 
 $MINIFI_DIR/minifi-toolkit/minifi-toolkit-0.5.0/bin/config.sh transform $HOME/mini-distr.xml $HOME/config.yml
 mv $HOME/config.yml $MINIFI_CONF
-
-. $HOME/.bashrc
