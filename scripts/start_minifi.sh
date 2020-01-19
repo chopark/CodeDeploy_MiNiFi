61ce9597760f3310d425ae5e6bde75782b50a7e4
ChanHo Park <pch8286@unist.ac.kr>
2020-01-19 20:20:34 +0900

    Fix wrong dir

--- scripts/start_minifi.sh
+++ scripts/start_minifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HOME="/home/ubuntu"
-MINIFI_DIR="$HOME/jarvis-minifi-scale-aws/minifi"
+MINIFI_DIR="$HOME/minifi"
 MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
 MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
