deda1c03138c88034094fd9b902a783c0fd27515
ChanHo Park <pch8286@unist.ac.kr>
2020-02-13 19:32:22 +0900

    Fix java installation

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,9 +10,7 @@ MINIFI_BIN="$MINIFI_HOME/bin"
 sudo apt-get update
 sudo apt-get install -y ca-certificates-java
 sudo apt-get install -y libxrandr2 libxinerama1 libgl1-mesa-glx libgl1 libgtk2.0-0 libasound2 libgif7 libpulse0
-sudo dpkg -i $MINIFI_DIR/jdk/*
-echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
-source /etc/environment
+
 
 #check minifi dir and extract from minifi.tar.gz
 if [ ! -d "$HOME/minifi" ]; then
@@ -20,5 +18,10 @@ if [ ! -d "$HOME/minifi" ]; then
         mv minifi $HOME/minifi
         sudo rm $HOME/minifi.tar.gz.*
 fi
+
+
+sudo dpkg -i $MINIFI_DIR/jdk/*
+echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+source /etc/environment
 
 sudo mv $HOME/config.yml $MINIFI_CONF
