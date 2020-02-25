714a467614785baf3c75311bf6b3976310c363ca
ChanHo Park <pch8286@unist.ac.kr>
2020-02-25 19:26:15 +0900

    Update splitted version

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -7,6 +7,7 @@ MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
 MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates-java
-sudo apt-get install -y libxrandr2 libxinerama1 libgl1-mesa-glx libgl1 libgtk2.0-0 libasound2 libgif7 libpulse0
+# install java
+sudo dpkg -i $MINIFI_DIR/jdk/*
+echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+source /etc/environment
