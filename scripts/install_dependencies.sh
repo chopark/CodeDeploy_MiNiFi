bb5085f538059ade136ca964e24458a7ea1ec7c7
ChanHo Park <pch8286@unist.ac.kr>
2020-02-25 20:18:00 +0900

    Update to add install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,3 +9,18 @@ MINIFI_BIN="$MINIFI_HOME/bin"
 
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
+
+#check minifi dir and extract from minifi.tar.gz
+if [ ! -d "$HOME/minifi" ]; then
+        cat $HOME/minifi.tar.gz.* | tar -zxvf -
+        mv minifi $HOME/minifi
+        sudo rm $HOME/minifi.tar.gz.*
+fi
+
+sudo mv $HOME/config.yml $MINIFI_CONF
+
+
+# install java
+sudo dpkg -i $MINIFI_DIR/jdk/*
+echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+source /etc/environment
