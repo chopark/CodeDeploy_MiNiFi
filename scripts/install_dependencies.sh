afe1304c68450c19aa4146b34256be4353ca0872
ChanHo Park <pch8286@unist.ac.kr>
2020-02-25 14:24:32 +0900

    Add flushing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,3 +10,7 @@ MINIFI_BIN="$MINIFI_HOME/bin"
 sudo apt-get update
 sudo apt-get install -y ca-certificates-java
 sudo apt-get install -y libxrandr2 libxinerama1 libgl1-mesa-glx libgl1 libgtk2.0-0 libasound2 libgif7 libpulse0
+
+echo "flush"
+echo "flush"
+echo "flush"
