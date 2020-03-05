839a3e2a030e331b51ba75e4befadf09ace884d1
pch8286 <pch8286@gmail.com>
2020-03-05 21:53:08 +0900

    Fix start op..

--- scripts/restart_minifi.sh
+++ scripts/restart_minifi.sh
@@ -6,6 +6,7 @@ MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
 
 if [ -d "$MINIFI_DIR" ]; then
+	sudo chmod +x $MINIFI_BIN/minifi.sh
 	$MINIFI_BIN/minifi.sh stop
 	rm -rf $MINIFI_HOME/content_repository/* $MINIFI_HOME/provenance_repository/* $MINIFI_HOME/flowfile_repository/* $MINIFI_HOME/state/local/* $MINIFI_HOME/logs/*
 	$MINIFI_BIN/minifi.sh start
--- scripts/status_minifi.sh
+++ scripts/status_minifi.sh
@@ -6,6 +6,7 @@ MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
 
 if [ -d "$MINIFI_DIR" ]; then
+	sudo chmod +x $MINIFI_BIN/minifi.sh
 	$MINIFI_BIN/minifi.sh status
 else
 	echo "$0: $MINIFI_DIR is missing."
