81f91119e6147c8c1a59a0aeffa20870d871dfb6
(대학원생) 박찬호 (컴퓨터공학과) <pch8286@unist.ac.kr>
2020-05-27 15:24:55 +0900

    noAdopt Add cpu limiter

--- scripts/start_minifi.sh
+++ scripts/start_minifi.sh
@@ -10,7 +10,11 @@ if [ -d "$MINIFI_DIR" ]; then
 	rm -rf $MINIFI_HOME/content_repository/* $MINIFI_HOME/provenance_repository/* $MINIFI_HOME/flowfile_repository/* $MINIFI_HOME/state/local/* $MINIFI_HOME/logs/*
 	$MINIFI_BIN/minifi.sh start
 	limit=$1
-	ps -ax | grep [o]rg.apache.nifi.minifi.MiNiFi | cut -d' ' -f2 | xargs -t cpulimit -l $limit -p
+	ps -ax | grep [o]rg.apache.nifi.minifi.MiNiFi | cut -d' ' -f2 | xargs -t timeout $2 cpulimit -l $limit -p
+	if [ $# -eq 3 ]; then
+			limit2=$3
+			ps -ax | grep [o]rg.apache.nifi.minifi.MiNiFi | cut -d' ' -f2 | xargs -t cpulimit -l $limit2 -p
+	fi
 else
 	echo "$0: $MINIFI_DIR is missing."
 fi
