612c7ffee71727a9cc8f1808492cb6453df07980
(대학원생) 박찬호 (컴퓨터공학과) <pch8286@unist.ac.kr>
2020-07-27 13:08:32 +0900

    Add java version maintainer

---
+++ scripts/install_java.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Initial setup to client.
+
+HOME="/home/ubuntu"
+MINIFI_DIR="$HOME/minifi"
+MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
+MINIFI_CONF="$MINIFI_HOME/conf"
+MINIFI_BIN="$MINIFI_HOME/bin"
+
+sudo dpkg -i $MINIFI_DIR/jdk/*
