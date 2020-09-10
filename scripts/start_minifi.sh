51b0db14c3bbe7285d4683f7f5609733b8ef05a6
ChanHo Park <pch8286@unist.ac.kr>
2020-09-10 20:33:41 +0900

    Snoop con2 4x minifi32

--- scripts/start_minifi.sh
+++ scripts/start_minifi.sh
@@ -5,13 +5,15 @@ MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
 MINIFI_CONF="$MINIFI_HOME/conf"
 MINIFI_BIN="$MINIFI_HOME/bin"
 
+
+
 if [ -d "$MINIFI_DIR" ]; then
 	sudo chmod +x $MINIFI_BIN/minifi.sh
 	rm -rf $MINIFI_HOME/content_repository/* $MINIFI_HOME/provenance_repository/* $MINIFI_HOME/flowfile_repository/* $MINIFI_HOME/state/local/* $MINIFI_HOME/logs/*
 	$MINIFI_BIN/minifi.sh start
 	if [ $# -eq 2 ]; then
 		limit=$2
-		sudo at $1 -f limit_cpu.sh $limit
+		sudo bash $HOME/scripts/limit_cpu.sh $limit | at $1 
 	fi
 else
 	echo "$0: $MINIFI_DIR is missing."
