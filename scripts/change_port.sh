2b0e3aed989f6523d7c6c6858ea009dad7b3e040
Ubuntu <ubuntu@ip-172-31-17-44.ec2.internal>
2021-02-12 09:32:12 +0000

    minifi custom config file update

--- scripts/change_port.sh
+++ scripts/change_port.sh
@@ -14,6 +14,6 @@ cp $CONF_DIR/from-minifi$1 ./config.yml
 sudo sed -i "s~minifi_custom.cfg~/home/ubuntu/minifi/minifi_custom.cfg~g" config.yml
 sudo sed -i "s~/home/ubuntu/minifi-repo/jarvis-minifi/minifi/operator-profile-input-data~/home/ubuntu/minifi/operator-profile-input-data~g" config.yml
 # sed -i "s~130.126.136.41~$IP~g" config.yml
-sudo sed -i "s~10.20.18.74~$IP~g" config.yml
+sudo sed -i "s~172.31.17.44~$IP~g" config.yml
 sudo sed -i "s~    max concurrent tasks: 1~    max concurrent tasks: 2~g" config.yml
 sudo mv ./config.yml $MINIFI_CONF
