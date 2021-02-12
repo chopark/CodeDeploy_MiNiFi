da51459e8f94202c314c5a6ce32c2f772f9c2cd6
Ubuntu <ubuntu@ip-172-31-17-44.ec2.internal>
2021-02-12 10:19:48 +0000

    deployment scripts with no getfile processor on minifi

--- scripts/change_port.sh
+++ scripts/change_port.sh
@@ -12,7 +12,7 @@ IP=$2
 cp $CONF_DIR/from-minifi$1 ./config.yml
 
 sudo sed -i "s~minifi_custom.cfg~/home/ubuntu/minifi/minifi_custom.cfg~g" config.yml
-sudo sed -i "s~/home/ubuntu/minifi-repo/jarvis-minifi/minifi/operator-profile-input-data~/home/ubuntu/minifi/operator-profile-input-data~g" config.yml
+sudo sed -i "s~/home/ubuntu/minifi-repo/jarvis-minifi/minifi/operator-profile-input-data~/home/ubuntu/minifi/operator-profile-input-data~g" $MINIFI_DIR/minifi_custom.cfg
 # sed -i "s~130.126.136.41~$IP~g" config.yml
 sudo sed -i "s~172.31.17.44~$IP~g" config.yml
 sudo sed -i "s~    max concurrent tasks: 1~    max concurrent tasks: 2~g" config.yml
